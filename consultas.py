333140c172215ecfffe6d3b9683b648e6ae15589
Henry Moreno <henrydmorenoe@gmail.com>
2020-12-16 12:38:47 -0500

    mejoras en conexion a DB

--- consultas.py
+++ consultas.py
@@ -1,18 +1,7 @@
-from db import conectar, ejecutar_consulta_sele, desconectar
+from db import consulta_seleccion, consulta_accion
 
-con = conectar()
-if con != None:
-    #sentencia = "SELECT nombres, apellidos, nickname, email, clave, estado FROM usuarios"    
-    sentencia =  """INSERT INTO usuarios(nombres, apellidos, estado, email, nickname, clave) VALUES() ;"""
-    res = ejecutar_consulta_sele(con, sentencia)
-    if res != None:
-        for fila in res:
-            print(fila)
-        #print("hola estoy aqui recuperando registros")
-    else:
-        print("No se selecciono ningun registro")
-    desconectar(con)
-else:
-    print("error al conectarse a la base de datos")
+datos = ('henry', 'moreno', 'A', '2henrymoreno@gmail.com', '2henrymoreno', 'Aa1234567' ) ## tupla o lista
+sentencia =  '''INSERT INTO usuarios (nombres, apellidos, estado, email, nickname, clave) VALUES(?,?,?,?,?,?) ;'''
+consulta_accion(sentencia, datos)
 
-
+print(consulta_seleccion("SELECT nombres, apellidos, nickname, email, clave, estado FROM usuarios"    ))
--- db.py
+++ db.py
@@ -5,55 +5,24 @@ def consulta_seleccion(sql):
     try:
         with sqlite3.connect( 'C:\\Users\\henda\\Downloads\\N U E  V  O\\proyectoCiclo03\\application\\tusAccesoriosApp.db') as conexion:
             cur = conexion.cursor()
-            sal = cur.execute()
+            sal = cur.execute(sql)
+            conexion.commit()
             if sal !=None:
                 sal = sal.fetchall()
     except:
         sal = None
     return sal
 
+##################
 
-
-def consulta_accion():
-    pass
-
-
-#######################
-
-def conectar():
+def consulta_accion(sql, datos):
     try:
-        sal = sqlite3.connect('C:\\Users\\henda\\Downloads\\N U E  V  O\\proyectoCiclo03\\application\\tusAccesoriosApp.db')
+        with sqlite3.connect( 'C:\\Users\\henda\\Downloads\\N U E  V  O\\proyectoCiclo03\\application\\tusAccesoriosApp.db') as conexion:
+            cur = conexion.cursor()
+            sal = cur.execute(sql, datos)
+            conexion.commit()
+            if sal>0:
+                print("se insertaa con exitos")
     except:
         sal = None
-    return sal
-
-######
-
-def desconectar(conexion):
-    try:
-        conexion.close()
-    finally:
-        con=None
-    return conexion
-
-##############
-
-def ejecutar_consulta_sele(con, sql):
-    try:
-        cur = con.cursor()
-        sal= cur.execute(sql)
-        if sal != None:
-            sal = sal.fetchall()
-    except:
-        sal = None
-    return sal
-
-######
-
-def ejecutar_consulta_accion(con, sql):
-    try:
-        cur = con.cursor()
-        sal = cur.execute(sql)
-    except :
-        sal = 0
     return sal
